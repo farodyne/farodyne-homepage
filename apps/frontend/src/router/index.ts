302162d839306aea04007ef5b9bf6dff2a43adf5
Federico Engler <github@farodyne.com>
2024-02-12 19:45:48 +0100

    Added view file structure.

--- apps/frontend/src/router/index.ts
+++ apps/frontend/src/router/index.ts
@@ -6,29 +6,29 @@
 import { h } from 'vue';
 import { createRouter, createWebHistory, RouterView } from 'vue-router';
 import { Routes } from '@/constants';
-// import { Album, Home, Section } from '@/views';
+import { album, home, section } from '@/views';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         // If no route is specified, default to the "home" route.
-        // { path: '', redirect: `/${Routes.Home}` },
+        { path: '', redirect: `/${Routes.home}` },
+
         // Let the "home" component render the view for the "home" route.
-        // { path: `/${Routes.Home}`, component: Home },
+        { path: `/${Routes.home}`, component: home },
+
         // For any other route that is not "home", it's either a section or
         // an album in the particular section. This rule is required to ensure
         // the correct router link in the navbar is highlighted as selected by
         // the applied active CSS class in the Vue Router.
-        /**
         {
             path: '/:section',
             component: { render: () => h(RouterView) },
             children: [
-                { path: '', component: Section },
-                { path: ':id', component: Album }
+                { path: '', component: section },
+                { path: ':id', component: album }
             ]
         }
-         */
     ]
 });
 
---
+++ apps/frontend/src/views/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Aggregate export file for views.
+ */
+import album from './album.vue';
+import home from './home.vue';
+import section from './section.vue';
+
+export { album, home, section };
